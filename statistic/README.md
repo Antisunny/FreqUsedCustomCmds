456ddee2d196bb30e496c79db4691b34d46c133e
rayrlee <antisunny@163.com>
2015-07-27 17:13:04 +0800

    add statistic

---
+++ statistic/README.md
@@ -0,0 +1,15 @@
+## F2 generation flowering time and fertility stat.
+> F2 of hen1-2 (early flowering) x hen1-8 (late flowering). both mutants have severe fertility defects.
+- 总共32盘
+- 每盘播种120株，但实际的发芽率和环境因素的影响，没有一盘是完整的
+- 最后只统计了3364株的相关参数，最后仍然有67株没有统计
+
+- data.txt
+	- `pan` 所在盘的数字编号
+	- `name` 每株植物的编号，唯一性
+	- `date` 主颈2cm时的日期，没有标明月份
+	- `rosette` 莲座叶
+	- `cauline`	分支叶
+	- `days` 主颈2cm距播种日期的时间(天数)
+- plot_out.R
+	- 
---
+++ statistic/plot_out.R
@@ -0,0 +1,27 @@
+getwd()
+all <- read.table("f2_gen_5.11.txt",header=TRUE,sep="\t")
+head(all)
+all <-  all[,c(1,2,4,5,6)]
+size <- dim(all)[1]
+a <- 0
+while(a< 32){
+	a <- a+1
+	this.pan <- all[all[,1]==a,]
+	this.pan.clean <- this.pan[!is.na(this.pan[,5]),]
+	lists <- c()
+	for (day in seq(from=23,to=50)){
+		count <- dim(this.pan.clean[this.pan.clean[,5]==day,])[1]
+		lists <- cbind(lists, count)
+	}
+	# for the pie plot
+	got <- dim(this.pan[!is.na(this.pan[,2]),])[1]/dim(this.pan)[1]
+	# days+count
+	startday <- 23 + min(which(lists != 0)) -1
+	curday <- 23 + max(which(lists != 0)) -1
+	# days+rosette/branch
+	this.pan.clean <- this.pan[!is.na(this.pan[,5]),]
+	names(this.pan.clean) <- c("pan", "name", "rosette","cauline","days")
+	select <- this.pan.clean[this.pan.clean[,3]>12 & this.pan.clean[,5]> curday-7,]
+	filename <- paste("/Users/admin/Advanced/gt12/",a,"-",curday,"-d.txt",sep="",collapse="")
+	write.csv(select,filename)
+}
